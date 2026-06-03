062482820e32d037c8227e004104a40e1a212177
masflame <tumishopraise03@gmail.com>
2026-06-03 19:29:38 +0200

    fix: use auth project for sign up/sign in instead of storage project

--- src/utils/authSupabase.js
+++ src/utils/authSupabase.js
@@ -1,12 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const authSupabaseUrl =
-  import.meta.env.VITE_STORAGE_PROJECT_URL ||
   import.meta.env.VITE_PROJECT_URL ||
   import.meta.env.VITE_SUPABASE_URL;
 
 const authSupabaseKey =
-  import.meta.env.VITE_STORAGE_PUBLISHABLE_KEY ||
   import.meta.env.VITE_PUBLISHABLE_KEY ||
   import.meta.env.VITE_SUPABASE_ANON_KEY;
 
